f92eb40f1971f29f5b0c3b4c234bc44cd3c8e666
Hakan Yıldırım <mail@hakanyildirim.com>
2017-12-31 14:14:02 +0300

    cevap01

---
+++ cevap01.java
@@ -0,0 +1,16 @@
+// Klavyeden 15 sayısı girilene kadar sayıları toplayıp sonucu ekrana yazan java programı
+package cevap01;
+import java.util.Scanner;
+public class cevap01 {
+    public static void main (String[] args) {
+        int toplam = 0, i = 1;
+        Scanner input = new Scanner(System.in);
+        while (i < 16) {
+            System.out.println(i + ". sayıyı giriniz: ");
+            int sayi = input.nextInt();
+            toplam+=sayi;
+            i++;
+        }
+        System.out.println("Girilen sayıların toplamı = " + toplam);
+    }
+}
